d25000ecb0281ba0a5b2df9c921dc42cfbf4e7d8
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-07-14 23:19:11 +0300

    Update tag.js

--- plugins/tag.js
+++ plugins/tag.js
@@ -8,7 +8,7 @@ const {getString} = require('./misc/lang');
 const {readFileSync} = require('fs');
 const {saveMessage} = require('./misc/saveMessage');
 const Lang = getString('group');
-Module({pattern: 'tag ?(.*)',use: 'group', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
+Module({pattern: 'تاك ?(.*)',use: 'group', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
 if (match[1] === "all" || !message.reply_message) return;
 var target = message.jid
 if (match[1] && /[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g.test(match[1])) target = [...match[1].match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)][0];
@@ -25,14 +25,14 @@ jids.push(user.id.replace('c.us', 's.whatsapp.net'));});
 }
 await message.forwardMessage(target,message.quoted,{detectLinks: true,contextInfo: {mentionedJid: jids}});
 }))
-Module({pattern: 'forward ?(.*)',use: 'utility', fromMe: true, desc: "Forwards message"}, (async (message, match) => {
+Module({pattern: 'توجيه ?(.*)',use: 'utility', fromMe: true, desc: "Forwards message"}, (async (message, match) => {
     if (!message.reply_message) return await message.sendReply("*Reply to a message*\n*Ex: .forward jid jid ...*")
     let Jids = [...match[1]?.match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)] || [message.jid]
         for (let jid of Jids) {
       await message.forwardMessage(jid, message.quoted,{detectLinks: true});
     }
 }));
-Module({pattern: 'send ?(.*)',use: 'utility', fromMe: true, desc: "Sends message"}, (async (message, match) => {
+Module({pattern: 'ارسال ?(.*)',use: 'utility', fromMe: true, desc: "Sends message"}, (async (message, match) => {
     if (!message.reply_message) return await message.sendReply("*Reply to a message*\n*Ex: .send jid jid ...*")
      if (!match[1]) match[1] = message.jid
     let Jids = [...match[1]?.match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)] || [message.jid];
