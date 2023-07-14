e9917c956c22993c6b6ff89744c4d7e74da317b4
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-07-14 21:04:41 +0300

    update onwa.js

--- plugins/onwa.js
+++ plugins/onwa.js
@@ -12,7 +12,7 @@ Module({
     usage: 'onwa +48888888xxx'
 }, (async (message, match) => {
     if (!match[1]) return await message.sendReply("_Need number!_");
-    let x = await message.send("_Processing.._");
+    let x = await message.send("_جـاري المعالجـة.._");
     await processOnwa(message,match[1])
     return await message.edit('_Task complete!_',message.jid,x.key)
 }));
